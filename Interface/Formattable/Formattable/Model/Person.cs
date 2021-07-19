11ef2f153ece410923752881b476ec86c07f8d39
chanos-dev <chanos.dev@gmail.com>
2021-07-19 22:18:02 +0900

    21-0718 (interface) - IFormattable

---
+++ Interface/Formattable/Formattable/Model/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formattable.Model
+{
+    public class Person : IFormattable
+    {
+        private const string DEFAULT_FORMAT = "P";
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Job { get; set; }
+
+        public override string ToString() => this.ToString(string.Empty, null);
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(format))
+                format = DEFAULT_FORMAT;
+
+            if (formatProvider is null)
+                formatProvider = CultureInfo.CurrentCulture;
+
+            switch (format)
+            {
+                case DEFAULT_FORMAT:
+                    return $"Name : {Name}\nAge : {Age} \nJob : {Job}";
+                case "N":
+                    return $"Name : {Name}";
+                case "A":
+                    return $"Age : {Age}";
+                case "J":
+                    return $"Job : {Job}";
+                default:
+                    throw new NotImplementedException("format is null");
+            }
+        }
+    }
+}
---
+++ Interface/Formattable/Formattable/Program.cs
@@ -0,0 +1,33 @@
+﻿using Formattable.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formattable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person()
+            {
+                Name = "Chanos",
+                Age = 24,
+                Job = "Programmer"
+            };
+
+
+            Console.WriteLine($"{p:N}");
+            Console.WriteLine("");
+            Console.WriteLine($"{p:A}");
+            Console.WriteLine("");
+            Console.WriteLine($"{p:J}");
+            Console.WriteLine("");
+            Console.WriteLine($"{p:P}");
+            Console.WriteLine("");
+            Console.WriteLine($"{p}");
+        }
+    }
+}
