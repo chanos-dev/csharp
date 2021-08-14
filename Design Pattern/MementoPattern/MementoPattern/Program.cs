4c47fd3297dea7a0f4795eb5727e9cce41b95e54
chanos-dev <chanos.dev@gmail.com>
2021-08-14 17:44:24 +0900

    MementoPattern

---
+++ Design Pattern/MementoPattern/MementoPattern/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Originator o = new Originator();
+            o.State = "On";
+
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+
+            o.State = "Off";
+
+            o.SetMemento(c.Memento);
+        }
+    }
+
+    public class Originator
+    {
+        private string _state;
+
+        public string State
+        {
+            get => _state;
+            set
+            {
+                _state = value;
+                Console.WriteLine($"State = {value}");
+            }
+        }
+
+        public Memento CreateMemento() => new Memento(_state);
+
+        public void SetMemento(Memento memento)
+        {
+            Console.WriteLine("Restoring state...");
+            State = memento.State;
+        }
+    }
+
+    public class Memento
+    {
+        private string _state;
+
+        public Memento(string state)
+        {
+            this._state = state;
+        }
+
+        public string State => _state;
+    }
+
+    public class Caretaker
+    {
+        public Memento Memento { get; set; }
+    }
+}
