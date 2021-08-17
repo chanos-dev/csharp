fd7230a471c936d177cfc54d968c378d14f7f7bb
chanos-dev <chanos.dev@gmail.com>
2021-08-18 00:37:54 +0900

    ProxyPattern

---
+++ Design Pattern/ProxyPattern/ProxyPattern/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            IFoo[] foos = new[]
+            {
+                new ProxyFoo("Hello"),
+                new ProxyFoo("World"),
+            };
+
+            foreach(var foo in foos)
+            {
+                foo.DoSomething();
+            }
+        }
+    }
+
+    interface IFoo
+    {
+        void DoSomething();
+    }
+
+    class Foo : IFoo
+    {
+        private string Data { get; set; }
+
+        public Foo(string data)
+        {
+            this.Data = data;
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine($"Data is {Data}.");
+        }
+    }
+
+    class ProxyFoo : IFoo
+    {
+        private Foo Foo { get; set; }
+        private string Data { get; set; }
+
+        public ProxyFoo(string data)
+        {
+            this.Data = data;
+        }
+
+        public void DoSomething()
+        {
+            if (Foo is null)
+                Foo = new Foo(Data);
+
+            Foo.DoSomething();
+        }
+    }
+}
