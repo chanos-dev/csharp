9bee41bb211ba06294719b06d6eb86bc5688b350
chanos-dev <chanos.dev@gmail.com>
2021-08-26 22:56:12 +0900

    CompositePattern

---
+++ Design Pattern/CompositePattern/CompositePattern/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompositePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Composite root = new Composite("root");
+            root.Add(new Leaf("Leaf A"));
+            root.Add(new Leaf("Leaf B"));
+
+            Composite comp = new Composite("Composite X");
+            comp.Add(new Leaf("Leaf XA"));
+            comp.Add(new Leaf("Leaf XB"));
+
+            root.Add(comp);
+            root.Add(new Leaf("Leaf C"));
+
+            Leaf leaf = new Leaf("Leaf D");
+            root.Add(leaf);
+            root.Remove(leaf);
+
+            root.Display(0);
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Component
+    {
+        protected string Name { get; set; }
+
+        public Component(string name)
+        {
+            this.Name = name;
+        }
+
+        public abstract void Add(Component component);
+        public abstract void Remove(Component component);
+        public abstract void Display(int depth);
+    }
+
+    public class Composite : Component
+    {
+        private List<Component> Children { get; set; } = new List<Component>();
+
+        public Composite(string name) : base(name)
+        {
+
+        }
+
+        public override void Add(Component component)
+        {
+            Children.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            Children.Remove(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine($"{new string('-', depth)}{Name}");
+
+            foreach (var child in Children)
+            {
+                child.Display(depth + 2);
+            }
+        }
+    }
+
+    public class Leaf : Component
+    {
+        public Leaf(string name) : base(name)
+        {
+
+        }
+
+        public override void Add(Component component)
+        {
+            Console.WriteLine("Cannot add to a leaf");
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine($"{new string('-', depth)}{Name}");
+        }
+
+        public override void Remove(Component component)
+        {
+            Console.WriteLine("Cannot remove from a leaf");
+        }
+    }
+}
