69c693c6c7283d4551553666bba82be0c9b6dcbe
chanos-dev <chanos.dev@gmail.com>
2021-09-05 21:41:26 +0900

    windows service app sample 작성

---
+++ Programming/WindowsServiceSample/WindowsServiceSample/SampleService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsServiceSample
+{
+    public partial class SampleService : ServiceBase
+    {
+        private Timer Timer { get; set; }
+
+        public SampleService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+            Timer = new Timer();
+            Timer.Interval = 2 * 60 * 1000;
+            Timer.Elapsed += Timer_Elapsed;
+            Timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var outPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), $"{DateTime.Now:d}.txt");
+
+            try
+            {
+                var client = new WebClient();
+                client.DownloadFile(@"http://www.naver.com", outPath);
+            }
+            catch (Exception ex)
+            {
+                var errorPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), $"{DateTime.Now:d}_error.txt");
+                File.WriteAllText(errorPath, ex.Message);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            Timer?.Stop();
+            Timer?.Dispose();
+        }
+    }
+}
