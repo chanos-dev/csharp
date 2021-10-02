89aceb918edac0be823dea58140e11304370fc78
chanos-dev <chanos.dev@gmail.com>
2021-10-02 20:08:40 +0900

    c# thread and delay

---
+++ Programming/SleepAndDelay/SleepAndDelay/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SleepAndDelay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sleep();
+
+            foreach (var i in Enumerable.Range(1,10))
+            {
+                Thread.Sleep(200);
+                Console.Write("1");
+            }
+
+            Delay();
+
+            foreach (var i in Enumerable.Range(1, 10))
+            {
+                Thread.Sleep(200);
+                Console.Write("2");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Sleep()
+        {
+            Thread.Sleep(1000);
+            Console.Write("Sleep");
+        }
+
+        static async void Delay()
+        {
+            await Task.Delay(1000);
+            Console.Write("Delay");
+        }
+    }
+}
