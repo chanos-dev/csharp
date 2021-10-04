3860bd63d357ad8c5c8b0358561fb98259b7cc45
chanos-dev <chanos.dev@gmail.com>
2021-10-04 21:19:19 +0900

    c# log4net

---
+++ Library/log4net/log4net/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log4net
+{
+    class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            Log.Info("info");
+            Log.Fatal("fatal");
+            Log.Error("error");
+            Log.Debug("Debug");            
+        }
+    }
+}
