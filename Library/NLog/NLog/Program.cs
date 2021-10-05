8f9cc02bb4856324fd50c601b2071c02cf565d1b
chanos-dev <chanos.dev@gmail.com>
2021-10-05 23:51:22 +0900

    c# nlog

---
+++ Library/NLog/NLog/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog
+{
+    class Program
+    {
+        private static readonly Logger Logger = LogManager.GetLogger("NLog");
+
+        static void Main(string[] args)
+        { 
+            Logger.Info("Info log");
+            Logger.Info("Info log2");
+
+            Logger.Warn("warning");
+
+            Logger.Fatal("fatal");
+        }
+    }
+}
