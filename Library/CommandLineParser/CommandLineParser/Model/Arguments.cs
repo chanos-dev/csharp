82f26d59bb17d683b43f53a69ec415a5547fecaa
chanos-dev <chanos.dev@gmail.com>
2021-12-19 21:25:41 +0900

    c# CommandLineParser

---
+++ Library/CommandLineParser/CommandLineParser/Model/Arguments.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParser.Model
+{
+    internal class Arguments
+    {
+        [Option('n', "name", Required = true, HelpText = "Set Name")]
+        public string Name { get; set; }
+
+        [Option('d', "dirs", Required = true, HelpText = "Set Directory Path")]
+        public string Directory { get; set; }
+
+        [Option('i', "ignore", Required = false, HelpText = "Ignore File.Exist Method")]
+        public bool Ignore { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name : {Name}\nDirectory : {Directory}\nIgnore : {Ignore}";
+        }
+    }
+}
---
+++ Library/CommandLineParser/CommandLineParser/Program.cs
@@ -0,0 +1,26 @@
+﻿using CommandLine;
+using CommandLineParser.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arguments = new Arguments();
+            Parser.Default.ParseArguments<Arguments>(args).WithParsed((a) =>
+            {
+                arguments.Name = a.Name;
+                arguments.Directory = a.Directory;
+                arguments.Ignore = a.Ignore;
+            });
+
+            Console.WriteLine(arguments);
+        } 
+    }
+}
