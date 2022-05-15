0aac11e5bf3f45ff4d8befb582a5f44a45b97e0d
chanos-dev <chanos.dev@gmail.com>
2022-05-16 02:43:18 +0900

    how to connect to debugger. - .NET6

---
+++ Other Patterns/Debugger/Debugger/Program.cs
@@ -0,0 +1,32 @@
+﻿#if DEBUG
+using System.Diagnostics;
+
+Console.WriteLine("Wait for connecting to a debugger for 10 seconds.");
+var th = new Thread(() =>
+{
+    try
+    {
+        while (!Debugger.IsAttached)
+        {
+            Console.WriteLine("wait..");
+            Thread.Sleep(1000);
+        }
+
+        Console.WriteLine("Success to connect to a debugger!!");
+    }
+    catch (ThreadInterruptedException tex)
+    {
+        Console.WriteLine("Interrupted..");
+        Console.WriteLine(tex.Message);
+    }
+});
+
+th.Start();
+// wait main thread..
+if (!th.Join(10 * 1000))
+{ 
+    th.Interrupt(); 
+}
+#endif 
+
+Console.WriteLine("Hello! .NET 6~~");
---
+++ Other Patterns/Debugger/Debugger_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+while (true)
+{
+    Console.WriteLine("the debugger command is to connect to a debugger process.");
+    Console.WriteLine("others command do nothing. ");
+    Console.Write("CMD : ");
+    var cmd = Console.ReadLine();
+    if (cmd is null) continue;
+    if (cmd.ToLower() == "debugger")
+        if (!Debugger.IsAttached)
+            Debugger.Launch();
+}
