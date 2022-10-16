3406c49272de6f2ba1b0c836f05760fc2d6eaf3c
chanos-dev <chanos.dev@gmail.com>
2022-10-16 21:19:05 +0900

    RabbitMQ tutorial hello world
    https://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html
    
    ## docker RabbitMQ
    
    docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:management
    
    - 기본 계정
      - ID : guest
      - PW : guest

---
+++ Programming/RabbitMQ/RabbitMQ/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Text; 
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+using var conn = factory.CreateConnection();
+using (var channel = conn.CreateModel())
+{ 
+    Receiving(channel);
+
+    while (true)
+    {
+        var input = Console.ReadLine();
+        if (string.Compare(input, "exit", true) == 0)
+        {
+            Console.WriteLine("exit..");
+            break;
+        }
+        
+        Send(input, channel);
+    }
+}
+
+void Send(string input, IModel channel)
+{
+    channel.QueueDeclare
+    (
+        queue: "ChanService1",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
+ 
+    var body = Encoding.UTF8.GetBytes(input!); 
+
+    channel.BasicPublish
+    (
+        exchange: "",
+        routingKey: "ChanService1",
+        basicProperties: null,
+        body: body
+    );
+
+    Console.WriteLine($"[x] Sent {input}");
+}
+
+void Receiving(IModel channel)
+{
+    channel.QueueDeclare
+    (
+        queue: "ChanService1",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine("[x] Received {0}", message);
+    };
+    
+    channel.BasicConsume
+    (
+        queue: "ChanService1",
+        autoAck: true,
+        consumer: consumer
+    ); 
+}
