177efbfa409bd6685ddf216250c2f89da1d5760e
chanos-dev <chanos.dev@gmail.com>
2023-01-29 22:31:02 +0900

    Hashids library example

---
+++ Library/Hashids/Program.cs
@@ -0,0 +1,20 @@
+﻿using HashidsNet;
+
+string salt = "chanos";
+string otherSalt = "chanoSS";
+
+Hashids hashids = new(salt);
+string enc = hashids.Encode(12345);
+Console.WriteLine($"Encode : {enc}");
+Console.WriteLine($"Decode : {string.Join(",", hashids.Decode(enc))}");
+
+Hashids otherIds = new(otherSalt);
+Console.WriteLine($"Decode : {string.Join(",", otherIds.Decode(enc))}");
+
+int[] arr = new[] {1,2,3,4,5};
+string arrEnc = hashids.Encode(arr);
+Console.WriteLine($"Encode : {arrEnc}");
+Console.WriteLine($"Decode : {string.Join(",", hashids.Decode(arrEnc))}");
+
+Hashids minhashids = new(salt, 10);
+Console.WriteLine($"Encode: {minhashids.Encode(1)}");
